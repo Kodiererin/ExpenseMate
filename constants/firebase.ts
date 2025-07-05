61522745af0704511b5940d4bbbded9b288285ff
Ujjwal <110911377+Kodiererin@users.noreply.github.com>
2025-07-05 12:52:39 +0530

    (Fix) : Minor typo

--- constants/firebase.ts
+++ constants/firebase.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore, Firestore } from "firebase/firestore";
+import { Firestore, getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
